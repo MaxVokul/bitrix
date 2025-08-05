28822459d0bfeeb9bdd53eb681325ca2b74c5694
MaxVokul <keny2880@gmail.com>
2025-08-05 20:22:50 +0300

    Задание 1.1 готово

--- partneram/.left.menu.php
+++ partneram/.left.menu.php
@@ -1,8 +1,22 @@
 <?
 $aMenuLinks = Array(
 	Array(
+		"Условия сотрудничества", 
+		"/partneram/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
 		"Как стать партнерам", 
 		"/partneram/kak-stat-partneram.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partneram/raspisanie-meropriyatiy/", 
 		Array(), 
 		Array(), 
 		"" 
--- partneram/raspisanie-meropriyatiy/index.php
+++ partneram/raspisanie-meropriyatiy/index.php
@@ -1,8 +1,8 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Как стать партнерам");
+$APPLICATION->SetTitle("Расписание мероприятий");
 ?>
 
-Text here....-ploplpl.;plop
+Text here....
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
